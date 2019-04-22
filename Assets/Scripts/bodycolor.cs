402eb242009f49c286abb931448c2192b9bf2a7b
Dylan Herrig <dylan.herrig@gmail.com>
2019-04-22 17:20:44 -0500

    moved walls

---
+++ Assets/Scripts/bodycolor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.ComponentModel;
+using UnityEngine.UI;
+
+public class bodycolor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Image PlayerBody;
+
+    void Start()
+    {
+        PlayerBody.color = PlayerGameManager.GetColor();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
